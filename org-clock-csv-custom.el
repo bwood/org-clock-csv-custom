bfa317c283f1d3b991df58283cef9667d451b0ba
Brian Wood <bwood@berkeley.edu>
2018-08-28 10:01:16 -0700

    Improve comment.

--- org-clock-csv-custom.el
+++ org-clock-csv-custom.el
@@ -1,5 +1,5 @@
+;; Override the autoloaded function org-clock-csv.
 ;; https://stackoverflow.com/a/15725437/844985
-
 (eval-after-load "org-clock-csv"
   '(defun org-clock-csv ()
      "Overridden org-clock-csv."
