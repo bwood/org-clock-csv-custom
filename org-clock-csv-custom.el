bebc039bbc5d5f401ee69918a69567c92b39be16
Brian Wood <bwood@berkeley.edu>
2018-09-11 10:12:26 -0700

    Get the global wps property for the buffer if no headline parent contains that property.

--- org-clock-csv-custom.el
+++ org-clock-csv-custom.el
@@ -85,7 +85,7 @@ Returns an empty string if no WPS billing property is found."
         (current element)
         (curlvl  (org-element-property :level element)))
     ;; If the headline does not have a wps property, recurse upwards
-    ;; through the parent headlines, checking if there is a category
+    ;; through the parent headlines, checking if there is a wps property
     ;; property in any of them.
     (while (not wpsbilling)
       (setq current (if (equal curlvl 1)
@@ -93,8 +93,10 @@ Returns an empty string if no WPS billing property is found."
                       (org-element-lineage current '(headline)))
             curlvl (- curlvl 1))
       (setq wpsbilling (org-element-property :WPS current))
-      ;; If we get to the root of the org file with no wps property, just
-      ;; set it to the empty string.
+      ;; If we get to the root of the org file with no wps property, 
+      ;; get the global wps property
+
+
       ;;
       ;; TODO: File-level categories are stored not as properties, but
       ;; as keyword elements in the `org-data' structure. In order to
@@ -103,8 +105,54 @@ Returns an empty string if no WPS billing property is found."
       ;; on an org file with no headline-level categories, but a
       ;; single file-level category, it would need to be cached.
       (unless (equal 'headline (org-element-type current))
-        (setq wpsbilling "")))
+        (if (org-global-prop-value "wps")
+	    (setq wpsbilling (org-global-prop-value "wps"))
+	  (setq wpsbilling "")))) ;;TODO open messasges buffer and alert the user that there is no WPS property
     wpsbilling))
 
 
+;; Code for getting/setting global properties for a buffer
+;; https://emacs.stackexchange.com/a/21472
+(require 'cl-lib)
 
+(defun org-global-props-key-re (key)
+  "Construct a regular expression matching key and an optional plus and eating the spaces behind.
+Test for existence of the plus: (match-beginning 1)"
+  (concat "^" (regexp-quote key) "\\(\\+\\)?[[:space:]]+"))
+
+(defun org-global-props (&optional buffer)
+  "Get the plists of global org properties of current buffer."
+  (with-current-buffer (or buffer (current-buffer))
+    (org-element-map (org-element-parse-buffer) 'keyword (lambda (el) (when (string-equal (org-element-property :key el) "PROPERTY") (nth 1 el))))))
+
+(defun org-global-prop-value (key)
+  "Get global org property KEY of current buffer.
+Adding up values for one key is supported."
+  (let ((key-re (org-global-props-key-re key))
+    (props (org-global-props))
+    ret)
+    (cl-loop with val for prop in props
+         when (string-match key-re (setq val (plist-get prop :value))) do
+         (setq
+          val (substring val (match-end 0))
+          ret (if (match-beginning 1)
+              (concat ret " " val)
+            val)))
+    ret))
+
+(defun org-global-prop-set (key value)
+  "Set the value of the first occurence of
+#+PROPERTY: KEY
+add it at the beginning of file if there is none."
+  (save-excursion
+    (let* ((key-re (org-global-props-key-re key))
+       (prop (cl-find-if (lambda (prop)
+                   (string-match key-re (plist-get prop :value)))
+                 (org-global-props))))
+      (if prop
+      (progn
+        (assert (null (match-beginning 1)) "First occurence of key %s is followed by +." key)
+        (goto-char (plist-get prop :begin))
+        (kill-region (point) (plist-get prop :end)))
+    (goto-char 1))
+      (insert "#+PROPERTY: " key " " value "\n"))))
